f4c662803c5d23cbf2fb1002617ea288980e1558
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-11 10:17:36 -0500

    Added maximum pairwise product in CPP

--- 1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ 1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,19 +2,43 @@
 #include <vector>
 #include <algorithm>
 
-int MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
-    int n = numbers.size();
+using namespace std;
 
-    for (int first = 0; first < n; ++first) {
-        for (int second = first + 1; second < n; ++second) {
-            max_product = std::max(max_product,
-                numbers[first] * numbers[second]);
+long long MaxPairwiseProduct(const vector<int> & numbers) {
+
+    int size = numbers.size();
+
+    int maxIndex1 = 0;
+    int maxIndex2 = size - 1;
+
+    for(int i = 1; i < size; i++) {
+        if (numbers[i] > numbers[maxIndex1]) {
+            maxIndex2 = maxIndex1;
+            maxIndex1 = i;
+        } else if (numbers[maxIndex2] < numbers[i]){
+            maxIndex2 = i;
         }
     }
 
-    return max_product;
+    // cout << numbers[maxIndex1] << endl;
+    // cout << numbers[maxIndex2] << endl;
+
+    return (long long) numbers[maxIndex1] * numbers[maxIndex2];
 }
+
+// int MaxPairwiseProduct_Slow(const std::vector<int>& numbers) {
+//     int max_product = 0;
+//     int n = numbers.size();
+
+//     for (int first = 0; first < n; ++first) {
+//         for (int second = first + 1; second < n; ++second) {
+//             max_product = std::max(max_product,
+//                 numbers[first] * numbers[second]);
+//         }
+//     }
+
+//     return max_product;
+// }
 
 int main() {
     int n;
@@ -24,6 +48,6 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers); << "\n";
+    std::cout << MaxPairwiseProduct(numbers) << "\n";
     return 0;
 }
