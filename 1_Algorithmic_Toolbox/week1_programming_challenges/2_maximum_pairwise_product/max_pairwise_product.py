d84e049ca88356b8794919d170089ad5cd4668ec
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-11 09:51:40 -0500

    Completed maximum_pairwise_product assignment in python

--- 1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ 1_Algorithmic_Toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -2,6 +2,21 @@
 
 
 def max_pairwise_product(numbers):
+
+    # Finding the first two maximum numbers in the list of numbers
+    max_number1 = float('-Inf')
+    max_number2 = float('-Inf')
+
+    for val in numbers:
+        if val > max_number1:
+            max_number2 = max_number1
+            max_number1 = val
+        elif val > max_number2:
+            max_number2 = val
+
+    return max_number1 * max_number2
+
+def max_pairwise_product_slow(numbers):
     n = len(numbers)
     max_product = 0
     for first in range(n):
@@ -10,7 +25,6 @@ def max_pairwise_product(numbers):
                 numbers[first] * numbers[second])
 
     return max_product
-
 
 if __name__ == '__main__':
     input_n = int(input())
