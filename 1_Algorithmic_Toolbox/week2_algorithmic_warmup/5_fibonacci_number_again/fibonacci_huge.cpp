bee954a3679abe101b9c6fd895f7553e540bb02d
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-16 00:25:21 -0500

    Added 5) Fibonacci Number Again Source Code

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 long long get_fibonacci_huge_naive(long long n, long long m) {
     if (n <= 1)
         return n;
@@ -16,8 +18,52 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     return current % m;
 }
 
+// For any integer m ≥ 2,
+// the sequence Fn mod m is periodic.
+// The period always starts with 01
+// and is known as Pisano period
+long long calculate_pisano_period_length(long long m) {
+    long long first = 0, second = 1, third = first + second;
+
+    for(long long i = 0; i < (m * m); i++) {
+        third = (first + second) % m;
+        first = second;
+        second = third;
+        if (first == 0 && second == 1) return i + 1;
+    }
+    return 2;
+}
+
+long long fibonacci_n_modulo_m(long long n, long m) {
+    if (n <= 1) {
+        return n;
+    }
+
+    if (n == 2) {
+        return 1;
+    }
+
+    long long prev = 0;
+    long long next = 1;
+
+    long long fibonacci_number = (prev + next) % m;
+
+    for (long long i = 2; i < n; i++) {
+        prev = next;
+        next = fibonacci_number;
+        fibonacci_number = (prev + next) % m;
+    }
+
+    return fibonacci_number % m;
+}
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    long long pisano_period_length = calculate_pisano_period_length(m);
+    return fibonacci_n_modulo_m(n % pisano_period_length, m);
+}
+
 int main() {
     long long n, m;
-    std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    cin >> n >> m;
+    cout << get_fibonacci_huge_fast(n, m) << '\n';
 }
