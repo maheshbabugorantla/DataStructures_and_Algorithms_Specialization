c4b2502e02c482368ee73ce100d1881c74e1ed2d
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-16 00:24:47 -0500

    Added 4) Least Common Multiple Source Code

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 long long lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
     if (l % a == 0 && l % b == 0)
@@ -8,9 +10,26 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int gcd(int a, int b) {
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+// a * b = LCM(a, b) * GCD(a, b)
+// LCM(a, b) = (a * b) / GCD(a, b)
+long long lcm_fast(int a, int b) {
+  return ((long long) a * b) / gcd(a, b);
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+
+  if (a < b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
