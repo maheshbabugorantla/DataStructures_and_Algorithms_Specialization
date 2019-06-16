28cd3972dd2d26b3e0e4d844e68b494f94fad71c
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-16 10:51:38 -0500

    Added faster method to compute the partial sum of fibonacci numbers from m to n

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+
+using namespace std;
 using std::vector;
 
 long long get_fibonacci_partial_sum_naive(long long from, long long to) {
@@ -21,8 +23,50 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+int get_fibonacci_partial_sum_fast(long long from, long long to) {
+
+    // If S(n) is the sum of fibonacci numbers from F0 to Fn
+    // If S(m-1) is the sum of fibonacci numbers from F0 to Fm-1
+    // Then, Fm + Fm+1 ... + Fn-1 + Fn = S(n) -  S(m-1)
+    // S(n) = Fn+2 - 1
+    // S(n) - S(m-1) = Fn+2 - Fm+1
+
+    if (from == 0 && to == 0) {
+        return 0;
+    }
+
+    // Fm+1 and Fn+2
+    int F_m1 = 0;
+    int F_n2 = 0;
+
+    int prev = 0;
+    int next = 1;
+
+    for(int i = 2; i < from + 2; i++) {
+        F_m1 = (prev % 10 + next % 10) % 10;
+        prev = next;
+        next = F_m1;
+    }
+
+    // cout << "Fm+1 = " << F_m1 << endl;
+
+    for(int i = from + 2; i < to+3; i++) {
+        F_n2 = (prev % 10 + next % 10) % 10;
+        prev = next;
+        next = F_n2;
+    }
+
+    // cout << "Fn+2 = " << F_n2 << endl;
+
+    if (F_n2 < F_m1) {
+        F_n2 = 10 + F_n2;
+    }
+
+    return (F_n2 - F_m1) % 10;
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
 }
