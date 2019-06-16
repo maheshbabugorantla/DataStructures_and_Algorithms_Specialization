1d190276f273ac5460048d251a28a53f43526b94
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-16 00:24:21 -0500

    Added 3) Greatest Common Divisor Source Code

--- 1_Algorithmic_Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ 1_Algorithmic_Toolbox/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 int gcd_naive(int a, int b) {
   int current_gcd = 1;
@@ -12,9 +13,23 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+// Euclidean GCD Algorithm
+// GCD(a, b) = GCD(a', b) = GCD(b, a')
+int gcd_fast(int a, int b) {
+  if (b == 0) return a;
+  return gcd_fast(b, a % b);
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+
+  if (a < b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+
+  std::cout << gcd_fast(a, b) << std::endl;
   return 0;
 }
