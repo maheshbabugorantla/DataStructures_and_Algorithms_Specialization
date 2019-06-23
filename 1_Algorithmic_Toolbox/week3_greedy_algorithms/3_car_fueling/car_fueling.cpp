02af87817e43e6d58d70dee278bf27f871bf32f6
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-23 10:52:22 -0500

    Completed the logic to compute minimum refills

--- 1_Algorithmic_Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ 1_Algorithmic_Toolbox/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -8,7 +8,50 @@ using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
     // write your code here
-    return -1;
+
+    if (tank >= dist) {
+        return 0;
+    }
+
+    if (tank < stops[0]) {
+        return -1;
+    }
+
+    int min_refills = 0;
+    int prev_stop = 0;
+
+    int gas_left_in_the_tank = tank;
+
+    for(unsigned int i = 0; i < stops.size(); i++) {
+        // cout << "Current Stop: " << stops[i] << std::endl;
+        int distance_covered = stops[i] - prev_stop;
+        gas_left_in_the_tank -= distance_covered;
+
+        if (gas_left_in_the_tank < 0) {
+            return -1;
+        }
+
+        if (gas_left_in_the_tank < (stops[i + 1] - stops[i])) {
+            // cout << "Refuelling" << std::endl;
+            min_refills += 1;
+            gas_left_in_the_tank = tank; // Refuel
+        }
+        prev_stop = stops[i];
+    }
+
+    // Covering the left over distance to the destination from the final gas stop
+    if (gas_left_in_the_tank < (dist - stops[stops.size() - 1])) {
+        // cout << "Refuelling at the final gas stop" << std::endl;
+        min_refills += 1;
+        gas_left_in_the_tank = tank; // Refuel at the final stop
+        gas_left_in_the_tank -= (dist - stops[stops.size() - 1]);
+    }
+
+    if (gas_left_in_the_tank < 0) {
+        return -1;
+    }
+
+    return min_refills;
 }
 
 
@@ -21,7 +64,7 @@ int main() {
     cin >> n;
 
     vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
         cin >> stops.at(i);
 
     cout << compute_min_refills(d, m, stops) << "\n";
