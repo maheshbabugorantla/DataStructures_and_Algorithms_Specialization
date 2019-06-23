ad5a20d42782d2f222c62c10107b2af57072a4c7
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-06-23 06:56:59 -0500

    Updated get_change method in change.cpp

--- 1_Algorithmic_Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
+++ 1_Algorithmic_Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
@@ -2,7 +2,11 @@
 
 int get_change(int m) {
   //write your code here
-  return n;
+  int coins_10 = m / 10;
+  m = m % 10;
+  int coins_5 = m / 5;
+  m = m % 5;
+  return m + coins_10 + coins_5;
 }
 
 int main() {
