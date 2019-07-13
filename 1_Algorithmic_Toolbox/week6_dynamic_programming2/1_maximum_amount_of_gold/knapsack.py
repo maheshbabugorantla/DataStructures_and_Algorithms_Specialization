a02ffbb6b5f2bdb567c9c42fc52706037e4be289
Mahesh Babu Gorantla <mgorantla@statestreet.com>
2019-07-13 18:59:19 -0500

    Submission Accepted

--- 1_Algorithmic_Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ 1_Algorithmic_Toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -2,12 +2,17 @@
 import sys
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    if not W:
+        return W
+
+    ks_table = [[0 for _ in range(W+1)] for _ in range(len(w)+1)]
+
+    for i in range(1, len(w) + 1):
+        for weight in range(1, W + 1):
+            ks_table[i][weight] = ks_table[i - 1][weight]
+            if w[i-1] <= weight:
+                ks_table[i][weight] = max(ks_table[i-1][weight-w[i-1]] + w[i-1], ks_table[i][weight])
+    return ks_table[len(w)][W]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
