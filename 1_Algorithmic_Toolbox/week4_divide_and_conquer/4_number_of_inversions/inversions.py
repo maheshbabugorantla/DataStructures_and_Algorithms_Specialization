b36f8a37f5d196f9a84fda3e0832c0ebe22e7609
Mahesh Babu Gorantla <mgorantla@statestreet.com>
2019-07-14 14:21:14 -0500

    Submission Accepted

--- 1_Algorithmic_Toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ 1_Algorithmic_Toolbox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -1,18 +1,48 @@
 # Uses python3
 import sys
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+def merge_sort(array):
+
+    if len(array) == 1:
+        return array, 0
+
+    mid = len(array) // 2
+
+    left = array[:mid]
+    right = array[mid:]
+
+    left, left_inversions = merge_sort(left)
+    right, right_inversions = merge_sort(right)
+
+    n_inversions = left_inversions + right_inversions
+
+    # Merge Step
+
+    i = 0
+    j = 0
+
+    merge_array = list()
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merge_array.append(left[i])
+            i += 1
+        else:
+            merge_array.append(right[j])
+            j += 1
+            n_inversions += (len(left) - i)
+
+    while i < len(left):
+        merge_array.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merge_array.append(right[j])
+        j += 1
+
+    return merge_array, n_inversions
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    print(merge_sort(a)[1])
