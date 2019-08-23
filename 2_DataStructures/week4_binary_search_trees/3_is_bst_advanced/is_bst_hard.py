d7dfb838c049bdde876ac00aa68270d04b9a18bb
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2019-08-23 10:19:55 -0500

    Submission Accepted

--- 2_DataStructures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ 2_DataStructures/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -5,17 +5,25 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+
+def is_bst(tree, root, min_val, max_val):
+  if len(tree) < 2:
+    return True
+
+  if root == -1:
+    return True
+  if tree[root][0] < min_val or tree[root][0] > max_val:
+    return False
+  return is_bst(tree, tree[root][1], min_val, tree[root][0]-1) and is_bst(tree, tree[root][2], tree[root][0], max_val)
 
 
 def main():
   nodes = int(sys.stdin.readline().strip())
   tree = []
+  keys = []
   for i in range(nodes):
     tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
+  if is_bst(tree, 0, float('-Inf'), float('Inf')):
     print("CORRECT")
   else:
     print("INCORRECT")
